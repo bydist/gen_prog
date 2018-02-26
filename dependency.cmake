321bf0947bd15a75f690e390d4d14d5df67ee407
David Callu <callu.david@gmail.com>
2018-02-26 14:27:07 +0100

    refactor(dependency): move boost to Boost to match cmake target

--- dependency.cmake
+++ dependency.cmake
@@ -3,8 +3,8 @@ include("${CMAKE_CURRENT_LIST_DIR}/id.cmake")
 
 
 byd__package__set_dependency(${package}
-    boost--boost
-    boost--filesystem
-    boost--system
-    boost--thread
+    Boost--boost
+    Boost--filesystem
+    Boost--system
+    Boost--thread
     )
